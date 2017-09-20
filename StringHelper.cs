153e33b3cdb4a986f993a2c399d3583e2abc2027
cstieg <cstieg4899@yahoo.com>
2017-09-20 11:10:27 -0400

    Initial commit

---
+++ StringHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace StringHelper
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Checks whether the string matches the wildcard pattern
+        /// </summary>
+        /// <param name="s">The present string object</param>
+        /// <param name="pattern">A wildcard pattern possible containing wildcards '*' and/or '?'</param>
+        /// <returns>True if a match, false if not</returns>
+        public static bool Matches(this String s, string pattern)
+        {
+            string regExPattern = "^" + Regex.Escape(s).Replace(@"\?", ".").Replace(@"\*", ".*") + "$";
+            return Regex.IsMatch(s, pattern);
+        }
+
+        public static bool ContainsWordWithWildcard(this String s, string word)
+        {
+            string regExPattern = ConvertWildcardStringToRegex(word);
+            return Regex.IsMatch(s, regExPattern);
+        }
+
+        public static MatchCollection FindWildcardMatches(this String s, string word)
+        {
+            string regExPattern = ConvertWildcardStringToRegex(word);
+            return Regex.Matches(s, regExPattern);
+        }
+
+        public static string ConvertWildcardStringToRegex(string s)
+        {
+            return @"\<" + Regex.Escape(s).Replace(@"\?", ".").Replace(@"\*", ".*") + @"\>";
+        }
+    }
+}
